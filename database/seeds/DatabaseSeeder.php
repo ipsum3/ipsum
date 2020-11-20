73112627877a694a5568a7470278c7943da1ec24
B1nj <postmaster@b1nj.fr>
2020-11-20 09:44:44 -0400

    add admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ArticleTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
+        $this->call(AdminsTableSeeder::class);
     }
 }
